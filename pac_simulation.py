edit code, correct inflation computation and change excel file generator engine

pac_simulation.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
 import base64
 import io
 
 # Dati input
 st.title('Simulatore PAC')
-st.markdown("Simula l'andamento di un PAC in base ai dati dei rendimenti reali medi annui imputati (il default è impostato sui valori del [CreditSuisse Yearbook](https://www.credit-suisse.com/about-us/it/reports-ricerca/studi-pubblicazioni.html)).")
+st.markdown("Simula l'andamento di un PAC in base ai dati dei rendimenti reali imputati (il default è impostato sui valori del [CreditSuisse Yearbook](https://www.credit-suisse.com/about-us/it/reports-ricerca/studi-pubblicazioni.html)).")
 
-numero_pac = st.number_input('Quanti PAC vuoi simulare?',step=1.0)
+numero_pac = st.number_input('Quanti PAC vuoi simulare?',step=1.0,value=1.0)
 
 keys = ['numero_asset','mesi','tempo','infla','azioni','bond_lt','bond_bt','versamento_mensile']
 dati_input = {key: {} for key in keys}
 
 final = pd.DataFrame()
+summary = pd.DataFrame(columns = ['versione pac','quota versamento mensile','capitale versato','montante finale','guadagno/perdita in €','guadagno/perdita in %'])
+
 ris = {}
 
 for i in range(int(numero_pac)):
@@ -24,7 +27,7 @@ st.header('Dati di input')
 for i in ris.keys():
     st.subheader('Inserisci i dati di input del ' + i)
     ris[i]['numero_asset'] = st.multiselect('Da quante asset class è costituisto il PAC',[1,2,3],[1], key=i)
-    ris[i]['tipo_asset'] = st.multiselect('Quale/quali asset class sono contenute nel PAC?',['Azioni','Bond','Materasso'],['Azioni'], key=i)
+    ris[i]['tipo_asset'] = st.multiselect('Quale/quali asset class sono contenute nel PAC?',['Azioni','Bond','Materasso (inflazione)'],['Azioni'], key=i)
     ris[i]['mesi'] = 12
     ris[i]['tempo'] = st.number_input('Per quanti anni rimane attivo il PAC',value=20, key=i)
     ris[i]['infla'] = st.number_input('Inserire target % di inflazione media annua, come proxy del "lasciare i soldi sotto al materasso"',value=2.0, key=i)
@@ -40,24 +43,25 @@ for i in ris.keys():
     rend_infla_mon = ((1+rend_infla_real)**(1/ris[i]['mesi']))-1
     
     v = [ris[i]['versamento_mensile']]*int(numero_pac)
-    ptf = np.zeros((1,ris[i]['mesi']*ris[i]['tempo']))
+    ptf = np.zeros((1,int(ris[i]['mesi']*ris[i]['tempo'])))
     
-    if ris[i]['numero_asset'] == 1:
-        if ris[i]['tipo_asset'] == 'Azioni':
+    if ris[i]['numero_asset'][0] == 1:
+        if ris[i]['tipo_asset'][0] == 'Azioni':
             rend_ptf = rend_az_mon
-        elif ris[i]['tipo_asset'] == 'Bond':
+        elif ris[i]['tipo_asset'][0] == 'Bond':
             rend_ptf = rend_bond_mon
         else:
             rend_ptf = rend_infla_mon
-    elif ris[i]['numero_asset'] == 2:
-        if ris[i]['tipo_asset'] == ['Azioni','Bond']:
+    elif ris[i]['numero_asset'][0] == 2:
+        if ris[i]['tipo_asset'][0] == ['Azioni','Bond']:
             rend_ptf = (rend_az_mon+rend_bond_mon)/ris[i]['numero_asset'][0]
-        elif ris[i]['tipo_asset'] == ['Azioni','Materasso']:
+        elif ris[i]['tipo_asset'][0] == ['Azioni','Materasso (inflazione)']:
             rend_ptf = (rend_az_mon+rend_infla_mon)/ris[i]['numero_asset'][0]
         else:
             rend_ptf = (rend_bond_mon+rend_infla_mon)/ris[i]['numero_asset'][0]
     else:
         rend_ptf = (rend_az_mon+rend_bond_mon+rend_infla_mon)/ris[i]['numero_asset'][0]
+    
     
     for z in range(ptf.shape[0]):
         for j in range(ptf.shape[1]):
@@ -68,18 +72,33 @@ for i in ris.keys():
     
     ptf = pd.DataFrame(ptf.T,columns=['nav'])
     st.write('Il versato per il pac mensile a',v[0],' euro è pari a: ',v[0]*ptf.shape[0],'euro in ',int(ptf.shape[0]/12),' anni. Il montante finale è pari a: ',ptf.tail(1).iloc[0].values[0],'euro.')
-    st.write('Il guadagno al netto dei versamenti è di: ',ptf.tail(1).iloc[0].values[0] - v[0]*ptf.shape[0],' euro, pari al ',((ptf.tail(1).iloc[0].values[0])/(v[0]*ptf.shape[0])-1)*100,'%')
+    st.write('Il guadagno/perdita al netto dei versamenti è di: ',ptf.tail(1).iloc[0].values[0] - v[0]*ptf.shape[0],' euro, pari al ',((ptf.tail(1).iloc[0].values[0])/(v[0]*ptf.shape[0])-1)*100,'%')
 
     
     ris[i]['time_series'] = ptf['nav'].to_dict()
     final = pd.concat([final, pd.DataFrame.from_dict(ris[i]['time_series'],orient='index')],axis=1)
     final = final.rename(columns={0:i})
     
+    summary.at[i,'versione pac'] = i
+    summary.at[i,'quota versamento mensile'] = v[0]
+    summary.at[i,'capitale versato'] = v[0]*ptf.shape[0]
+    summary.at[i,'montante finale'] = ptf.tail(1).iloc[0].values[0]
+    summary.at[i,'guadagno/perdita in €'] = ptf.tail(1).iloc[0].values[0] - v[0]*ptf.shape[0]
+    summary.at[i,'guadagno/perdita in %'] = ((ptf.tail(1).iloc[0].values[0])/(v[0]*ptf.shape[0])-1)*100
+    
+buffer = io.BytesIO()
+with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    final.to_excel(writer,'serie_storica')
+    summary.to_excel(writer,'sommario')
+    writer.save()
+    st.download_button(label="Scarica il file con i dati delle simulazioni!",data=buffer,file_name="pac_simulation.xlsx",mime="application/vnd.ms-excel")
 
-            
-towrite = io.BytesIO()
-downloaded_file = final.to_excel(towrite, encoding='utf-8', index=False, header=True)
-towrite.seek(0)  
-b64 = base64.b64encode(towrite.read()).decode()  
-linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="pac_simulation.xlsx">Download excel file</a>'
-st.markdown(linko, unsafe_allow_html=True)
+# towrite = io.BytesIO()
+# downloaded_file = final.to_excel(towrite, encoding='utf-8', index=False, header=True)
+# towrite.seek(0)  
+# b64 = base64.b64encode(towrite.read()).decode()  
+# linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="pac_simulation.xlsx">Download excel file</a>'
+# st.markdown(linko, unsafe_allow_html=True)
+
+
+